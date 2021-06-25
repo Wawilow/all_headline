39f516dabbb3a005bb55ff14981aea7f366bd3d8
Admin <ujif987@mail.ru>
2021-06-26 01:35:38 +0300

    все заработало, добавил информацию о компютере. доработал цикл, пока что работает быстро, возможно потом разпаралелю потоки что бы быстрее было

--- parsim.py
+++ parsim.py
@@ -3,39 +3,62 @@ from lxml import html
 from  sql_work import sql
 
 
-def pats(url):
-    global i
+def pats(url, headers):
     # headline, discription, url, somphing
     # headline
     # /html/body/div[6]/div[1]/div[1]/div[1]/div[2]/h1/text()
     # /html/body/div[6]/div[1]/div[1]/div[1]/div[2]/h1
     # discription
     # /html/body/div[6]/div[1]/div[1]/div[1]/div[3]/div[9]/text()
-    #
-    print('work')
-    i += 1
-    request = requests.get(url)
-    tree = html.document_fromstring(request.text)
-    headline = tree.xpath(f'/html/body/div[6]/div[1]/div[1]/div[1]/div[2]/h1/text()')
-    discription = tree.xpath(f'/html/body/div[6]/div[1]/div[1]/div[1]/div[3]/div[9]/text()')
-    print(i, headline, discription)
-    print(sql(headline, discription, url, ''))
+
+    print(f'work with url:{url}')
+    try:
+        response = requests.get(url, headers=headers)
+        tree = html.document_fromstring(response.text)
+        headlines = tree.xpath(f'//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/h1/text()')
+        discription = tree.xpath(f'//*[@id="content"]/div[1]/div[1]/div[1]/div[3]/div[9]/text()')
+        if headlines == []:
+            return False
+        if discription == []:
+            return False
+    except:
+        return False
+    sql_add = sql(headlines, discription, url, i)
+    if sql_add[-1] == True:
+        return (f"work with {url} is good, and we hav that {headlines}"
+              f" and {discription}")
+    else:
+        return f"{sql_add}"
 
 
-def test_past(url):
-    # //*[@id="content"]/div[1]/div[1]/div[1]/div[2]/h1
-    global i
-    request = requests.get(url)
-    tree = html.document_fromstring(request.text)
-    headline = None
-    print(tree.xpath('/html/body/div[4]/section[2]/div[2]/div/div[1]/section/div[1]/a/h3/text()'))
-    print(headline)
+def test_past(url, headers):
+    response = requests.get(url, headers=headers)
+    tree = html.document_fromstring(response.text)
+    headlines = tree.xpath(f'//*[@id="content"]/div[1]/div[1]/div[1]/div[2]/h1/text()')
+    discription = tree.xpath(f'//*[@id="content"]/div[1]/div[1]/div[1]/div[3]/div[9]/text()')
+
 
 def main():
-    #test_past(f'http://porno365.lol/movie/{i}')
-    test_past(f'https://vk.com/interestingstatistic')
+    global i
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+        'Referer': 'http://porno365.lol/'
+    }
+    work = pats(f'http://porno365.lol/movie/{i}', headers)
+    print(work)
+    if work == False:
+        return False
+    #test_past(f'http://porno365.lol/movie/2', headers)
+    i += 1
 
 
 if __name__ == '__main__':
-    i = 2
-    main()
+    i = 1
+    kafizient = True
+    while kafizient:
+        mai = main()
+        if mai == False:
+            kafizient = False
+
+
