706a9c92553ea83553d78a822d6cb25057020e90
Admin <ujif987@mail.ru>
2021-06-26 01:36:12 +0300

    тестовый файл

---
+++ fake_person.py
@@ -0,0 +1,14 @@
+import fake_useragent
+import requests
+
+
+# test file, here i try to solve the connect problem
+# when i get with url without headers, received a forbidden error
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Referer': 'http://porno365.lol/'
+}
+url = "http://porno365.lol"
+response = requests.get(url, headers=headers)
+print(response.text)
