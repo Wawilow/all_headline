8e080f87bf6436040abc373f0ec5719ff25bfa33
Admin <ujif987@mail.ru>
2021-06-27 00:23:50 +0300

    make random for information about pc make real good cycle, he dont need human hints i write after

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 from parsim import pats
 from parsim import test_past
 from sql_work import last_in_base
+from random import randrange
+import time
+
+def random_headers(rand):
+    if rand == True:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+            'Referer': 'http://porno365.lol/'
+        }
+    else:
+        to5 = randrange(0, 50) / 10
+        to10 = randrange(0, 9)
+        to100 = randrange(0, 99)
+        to1000 = randrange(0, 999)
+        to10000 = randrange(0, 9999)
+        headers = {
+            'User-Agent': f'Mozilla/{to5} (Macintosh; Intel Mac OS X {to10}_{to10}) AppleWebKit/{to1000}.{to100} '
+                          f'(KHTML, like Gecko) Chrome/{to100}.{to10}.{to10000}.{to1000} Safari/{to1000}.{to100}',
+            'Referer': 'http://porno365.lol/'
+        }
+    return headers
+
+
 
 def main():
     global i
-    headers = {
-        'User-Agent': 'Mozilla/4.9 (Macintosh; Intel Mac OS X 11_3) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/92.0.4573.104 Safari/537.37',
-        'Referer': 'http://porno365.lol/'
-    }
+    headers = random_headers(True)
     work = pats(f'http://porno365.lol/movie/{i}', headers, i)
     print(work)
     if work[0] == False:
@@ -18,9 +38,30 @@ def main():
 
 
 if __name__ == '__main__':
-    i = last_in_base() + 1
-    kafizient = True
-    while kafizient:
-        mai = main()
-        if mai == False:
-            kafizient = False
+    how_many_fals = 0
+    plus = 1
+    # 298
+    TR = True
+    while TR:
+        if how_many_fals <= 10:
+            print(f'сколько было ошибок {how_many_fals}')
+            how_many_fals = 0
+            plus += 1
+            print(f'сколько плюсуем {plus}')
+        try:
+            i = last_in_base() + plus
+            kafizient = True
+            while kafizient:
+                mai = main()
+                if mai == False:
+                    kafizient = False
+                    print(mai)
+                    int('q')
+                else:
+                    plus = 1
+                    how_many_fals = 0
+        except:
+            how_many_fals += 1
+            #print(f'time to sleep')
+            time.sleep(120)
+
